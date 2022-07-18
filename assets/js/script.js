a60d27c2eec706d9f88496d6bc76273e0ad5c4a0
Koober97 <koober@Brians-MacBook-Pro.local>
2022-07-18 19:13:05 -0400

    start of api integration

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,22 @@
+const api_url = "https://api.waifu.pics/sfw/cringe"
 
 $(document).ready(function(){
     $('.dropdown-trigger').dropdown({
         coverTrigger: false
     });
 });
+
+fetch('https://api.waifu.pics/sfw/cringe')
+    .then((response) => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error("NETWORK RESPONSE ERROR");
+        }
+    })
+    .then(data => {
+        console.log(data);
+        document.querySelector("waifu").innerHTML = '<h3>hello</h3>'
+    })
+    .catch((error) => console.error("FETCH ERROR:", error));
+
